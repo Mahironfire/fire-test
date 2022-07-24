8bca3fe8bebc30211eb245fea02c89e2d221c4c0
DOD-99 <106377151+Mahironfire@users.noreply.github.com>
2022-07-24 11:28:29 +0600

    Update fire-ok.py

--- fire-ok.py
+++ fire-ok.py
@@ -389,7 +389,7 @@ def follow(ses,coki):
 
 logo = ("""\033[1;96m
        
- __  __       _     _       __      __   _
+  __  __       _     _       __      __   _
  |  \/  |     | |   (_)      \ \    / /  (_)
  | \  / | __ _| |__  _ _ __   \ \  / /_ _ _
  | |\/| |/ _` | '_ \| | '__|   \ \/ / _` | |
@@ -417,7 +417,7 @@ class Main:
 		print(logo)
 		print("\n \x1b[1;93m [1] File Cloning")
 		print(" \x1b[1;96m [2] Public Cloning")
-		print(" \x1b[1;91m [3] 2009-10 Cloning")
+		print(" \x1b[1;97m [3] 2009-10 Cloning")
 		print(" \x1b[1;98m [4] 2011-14 Cloning")
 		print(" \x1b[1;94m [E] Exit Programming\n")
 		MAHIR =input(" Choose : ")
